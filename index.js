4613e36046cd7baded6cc90e1cd406520362343b
Raphael Reges Valente <valente@MacBook-Pro-de-Raphael.local>
2022-05-28 22:24:05 -0300

    config file router

--- index.js
+++ index.js
@@ -2,5 +2,6 @@ const express = require('express');
 const app = express();
 const routers = require('./scr/routers/routers');
 
-app.use(routers);
-app.listen(4001, () => { console.log('Server is running on port 4001') });
+app.use(routers)
+    .use(express.json())
+    .listen(4001, () => { console.log('Server is running on port 4001') });
